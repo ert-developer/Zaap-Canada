01e7d60d8b3996b7853cd679eee9e04cf6b01466
Aditya Dasari <aditya.dnvks@gmail.com>
2025-01-11 18:37:10 +0530

    Added camera feature for chats

--- src/pages/chats/message/message-screen.js
+++ src/pages/chats/message/message-screen.js
@@ -1,7 +1,7 @@
 import database from '@react-native-firebase/database';
-import {useNavigation} from '@react-navigation/native';
 import React, {useEffect, useMemo, useRef, useState} from 'react';
 import {
+  ActivityIndicator,
   Animated,
   FlatList,
   Image,
@@ -11,6 +11,7 @@ import {
   StatusBar,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import {useSelector} from 'react-redux';
@@ -25,6 +26,12 @@ import {getUserDetails, postCollectionDetails} from '../../../common/collection'
 import {envConfig} from '../../../assets/helpers/envApi';
 import moment from 'moment';
 import {PUSH_NOTIFICATION_SERVER_URL} from '@env';
+import CustomModelComponent from '../../../atoms/model/model-component';
+import ButtonIconLabelComponent from '../../../atoms/buttonIconlabel/buttonIconlabel-component';
+import {CameraIcon, GalleryIcon} from '../../../assets/svgIcons/postJob';
+import {uploadImage} from '../../../common/camera';
+import CustomButton from '../../../atoms/button/buttonComponent';
+import storage from '@react-native-firebase/storage';
 
 const ChatScreenExample = ({data, allChat}) => {
   const user = useSelector(state => state.Auth.user);
@@ -32,6 +39,7 @@ const ChatScreenExample = ({data, allChat}) => {
 
   const {photoURL, imageUrl, isServiceProvider} = data;
   const styles = useMemo(() => ChatStyles(), []);
+  const [modalVisible, setModalVisible] = useState(false);
   const [messages, setMessages] = useState(allChat);
   const [newMessage, setNewMessage] = useState('');
   const fadeAnim = useRef(new Animated.Value(0)).current;
@@ -88,17 +96,25 @@ const ChatScreenExample = ({data, allChat}) => {
       <Animated.View style={messageStyle}>
         {item.from === user.userId && (
           <View style={{flexDirection: 'column'}}>
-            <Text
-              style={{
-                backgroundColor: item.from !== user.userId ? '#E6E6E6' : '#5A2DAF',
-                padding: Padding.p_14,
-                color: item.from !== user.userId ? Color.colorBlack : Color.colorWhite,
-                borderRadius: Border.br_16,
-                overflow: 'hidden',
-                maxWidth: widthToDp(70),
-              }}>
-              {item.message}
-            </Text>
+            {item.typeOfMessage === 'text' ? (
+              <Text
+                style={{
+                  backgroundColor: item.from !== user.userId ? '#E6E6E6' : '#5A2DAF',
+                  padding: Padding.p_14,
+                  color: item.from !== user.userId ? Color.colorBlack : Color.colorWhite,
+                  borderRadius: Border.br_16,
+                  overflow: 'hidden',
+                  maxWidth: widthToDp(70),
+                }}>
+                {item.message}
+              </Text>
+            ) : (
+              <Image
+                source={{uri: item.message}}
+                style={{width: widthToDp(50), height: heightToDp(20), borderRadius: 10}}
+              />
+            )}
+
             <Text style={{...styles.timeStamp, textAlign: 'right', marginRight: 5}}> {formattedTime}</Text>
           </View>
         )}
@@ -116,17 +132,25 @@ const ChatScreenExample = ({data, allChat}) => {
 
         {item.from !== user.userId && (
           <View style={{flexDirection: 'column'}}>
-            <Text
-              style={{
-                backgroundColor: item.from !== user.userId ? '#E6E6E6' : '#5A2DAF',
-                padding: Padding.p_14,
-                color: item.from !== user.userId ? Color.colorBlack : Color.colorWhite,
-                borderRadius: Border.br_16,
-                overflow: 'hidden',
-                maxWidth: widthToDp(70),
-              }}>
-              {item.message}
-            </Text>
+            {item.typeOfMessage === 'text' ? (
+              <Text
+                style={{
+                  backgroundColor: item.from !== user.userId ? '#E6E6E6' : '#5A2DAF',
+                  padding: Padding.p_14,
+                  color: item.from !== user.userId ? Color.colorBlack : Color.colorWhite,
+                  borderRadius: Border.br_16,
+                  overflow: 'hidden',
+                  maxWidth: widthToDp(70),
+                }}>
+                {item.message}
+              </Text>
+            ) : (
+              <Image
+                source={{uri: item.message}}
+                style={{width: widthToDp(50), height: heightToDp(20), borderRadius: 10}}
+              />
+            )}
+
             <Text style={{...styles.timeStamp, textAlign: 'left', marginLeft: 5}}>{formattedTime}</Text>
           </View>
         )}
@@ -189,6 +213,64 @@ const ChatScreenExample = ({data, allChat}) => {
     fadeIn();
   };
 
+  const handleImage = sourceType => {
+    uploadImage(sourceType, handleImageResponse, 1);
+  };
+
+  const [imagesLoader, setImagesLoader] = useState(false);
+
+  const handleImageResponse = async response => {
+    if (response.didCancel) {
+      console.log('User cancelled image picker');
+    } else if (response.error) {
+      console.log('Image picker error: ', response.error);
+    } else {
+      try {
+        setImagesLoader(true); // Start loader
+
+        if (response.assets) {
+          // Calculate the number of slots left for new images
+          const remainingSlots = 1;
+          const newImages = response.assets.slice(0, remainingSlots); // Limit the number of images based on remaining slots
+          const imageUrls = [];
+
+          await Promise.all(
+            newImages.map(async image => {
+              const storageRef = storage().ref(`${envConfig.images}/${Date.now()}-${image.fileName}`);
+              await storageRef.putFile(image.uri);
+              const downloadURL = await storageRef.getDownloadURL();
+              imageUrls.push(downloadURL);
+            }),
+          );
+
+          const messageData = {
+            roomId: data.roomId,
+            message: imageUrls[0],
+            from: user.userId,
+            to: data.userId,
+            typeOfMessage: 'img',
+            sendTime: new Date().toISOString(), // Convert date to ISO string
+            markasread: false,
+          };
+
+          const newReference = database().ref(`/${envConfig.message}/${data.roomId}`).push();
+
+          newReference.set(messageData).then(() => sendNotification(messageData));
+          const updatedMessages = [...messages, {id: 1, sender: 'user', message: imageUrls[0]}];
+          setMessages(updatedMessages);
+          setNewMessage('');
+          scrollViewRef.current.scrollToEnd({animated: true});
+          fadeIn();
+          setModalVisible(false);
+        }
+      } catch (error) {
+        console.error('Error uploading image to Firebase:', error);
+      } finally {
+        setImagesLoader(false); // Stop loader after the process is done
+      }
+    }
+  };
+
   const renderAvatar = () => {
     if (isServiceProvider === false || isServiceProvider === 'no') {
       if (imageUrl) {
@@ -224,6 +306,33 @@ const ChatScreenExample = ({data, allChat}) => {
           contentContainerStyle={{flexGrow: 1}}
           ref={scrollViewRef}>
           <View style={styles.container}>
+            <View style={styles.row}></View>
+            <CustomModelComponent modalVisible={modalVisible} setModalVisible={setModalVisible}>
+              <View style={styles.centeredView}>
+                {imagesLoader ? (
+                  <ActivityIndicator size="large" color={Color.colorIndigo2} /> // Add margin for spacing
+                ) : (
+                  <View style={styles.modalView}>
+                    <CustomText text={'Upload Photo'} style={styles.selectPhotoTitle} />
+                    <View style={styles.row}>
+                      <ButtonIconLabelComponent handlePress={() => handleImage('camera')} label={'Camera'}>
+                        <CameraIcon width="35px" height="35px" />
+                      </ButtonIconLabelComponent>
+
+                      <ButtonIconLabelComponent handlePress={() => handleImage('gallery')} label={'gallery'}>
+                        <GalleryIcon width="35px" height="35px" />
+                      </ButtonIconLabelComponent>
+                    </View>
+                    <CustomButton
+                      title="Cancel"
+                      onPress={() => setModalVisible(!modalVisible)}
+                      style={styles.cancelButton}
+                      textStyle={styles.cancelTextStyle}
+                    />
+                  </View>
+                )}
+              </View>
+            </CustomModelComponent>
             <View>
               {renderAvatar()}
               <CustomText text={data.displayName} style={styles.displayName} />
@@ -259,7 +368,17 @@ const ChatScreenExample = ({data, allChat}) => {
 
             {/* Actions: Camera Icon + Send Button */}
             <View style={[styles.row, styles.camsend]}>
-              <Camera />
+              <TouchableOpacity
+                style={[
+                  styles.addPhotoButton,
+                  imagesLoader && styles.disabledButton, // Optional style when loading
+                ]}
+                onPress={() => !imagesLoader && setModalVisible(true)} // Disable button if loading
+                disabled={false} // Disable interaction
+              >
+                <Camera />
+              </TouchableOpacity>
+
               <CustomTouchableOpacity
                 onPress={() => {
                   handleSend();
